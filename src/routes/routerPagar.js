1e749396d61ca1ea94370b890a07313d76b4c73b
juan rios <juan.rios49@eia.edu.co>
2020-11-10 18:37:40 -0500

    payments

--- src/routes/routerPagar.js
+++ src/routes/routerPagar.js
@@ -4,9 +4,9 @@ const  controlPagar=require('../controllers/controllerPagar')
 const protectedRoutes=require('../helpers/protectedRoutes')
 
 route.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin","https://glacial-everglades-42121.herokuapp.com/")
     res.header("Access-Control-Allow-Origin","https://peaceful-ridge-86113.herokuapp.com/")
     res.header("Access-Control-Allow-Origin","http://localhost:3000/")
+    res.header("Access-Control-Allow-Origin","https://frozen-anchorage-52618.herokuapp.com/")
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS");
     next()
